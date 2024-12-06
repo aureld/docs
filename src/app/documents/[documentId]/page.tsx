a80cf88f7fe55f46a7a5af19a9251a1afb7f7cd5
Aurelien <aurelien@aduval.me>
2024-12-06 22:54:03 +0100

    editor store and undo

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,9 +1,18 @@
+import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
+
 interface DocumentIdPageProps {
-    params: Promise<{documentId: string }>
+  params: Promise<{ documentId: string }>;
 }
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-    const {documentId} = await params
-    return ( <div>Document Id: {documentId} </div> );
-}
- 
+  const { documentId } = await params;
+
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
+      <Editor />
+    </div>
+  );
+};
+
 export default DocumentIdPage;
