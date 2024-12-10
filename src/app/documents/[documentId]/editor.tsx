f7d360b26b84cc49c2fed04ad485e617950bddea
Aurelien <aurelien@aduval.me>
2024-12-10 11:11:09 +0100

    align and lists

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,7 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import TextAlign from "@tiptap/extension-text-align";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
 import Table from "@tiptap/extension-table";
@@ -70,6 +71,9 @@ export const Editor = () => {
       Underline,
       Color,
       Highlight.configure({ multicolor: true }),
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
     ],
     content: `
         <table>
