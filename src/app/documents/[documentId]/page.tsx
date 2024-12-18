8fcaf98ccc8f754bdcf960cc351c3c20ab1411aa
Aurelien <aurelien@aduval.me>
2024-12-18 13:38:46 +0100

    navbar menus

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,16 +1,20 @@
 import { Editor } from "./editor";
+import { Navbar } from "./navbar";
 import { Toolbar } from "./toolbar";
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
-const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-  const { documentId } = await params;
-
+const DocumentIdPage = async ({}: DocumentIdPageProps) => {
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
-      <Toolbar />
-      <Editor />
+      <div className="flex flex-col px-4 pt-4 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden">
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className="pt-[114px] print:pt-0">
+        <Editor />
+      </div>
     </div>
   );
 };
