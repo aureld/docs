5ecb621819081d7831628be76e20e56a930c4069
Aurelien Duval <aurelien@aduval.me>
2025-01-30 23:07:04 +0100

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://endless-sturgeon-5.clerk.accounts.dev",
+      domain: "https://editme",
       applicationID: "convex",
     },
   ],
